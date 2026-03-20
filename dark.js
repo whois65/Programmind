c0a104be6354f0958bf5840ed68cbd3eb7537878
Raffiqi <raffi652009@gmail.com>
2026-03-20 18:19:18 +0700

    Add files via upload

---
+++ dark.js
@@ -0,0 +1,8 @@
+function toggleDark() {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("darkMode", document.body.classList.contains("dark"));
+}
+
+if (localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark");
+}
