d742be096ea75c7cf0a6a0e6f503b32ea497389e
whois65 <raffi652009@gmail.com>
2026-06-27 01:02:02 +0700

    Fix data structures

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,21 @@ const qs = (x) => document.querySelector(x);
 
 function toggleDark() {
   document.body.classList.toggle("dark");
-  localStorage.setItem("darkMode", document.body.classList.contains("dark"));
+  let a = document.body.classList.contains("dark");
+  if (a) {
+    qi("toggleDarkIcon").classList = "ti ti-sun";
+    qi("toggleDarkIcon").style = "color: #fff;";
+  } else {
+    qi("toggleDarkIcon").classList = "ti ti-moon";
+    qi("toggleDarkIcon").style = "color: #000;";
+  }
+  localStorage.setItem("darkMode", a);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("darkMode") === "true") {
     document.body.classList.add("dark");
-    qi("ToggleDarkicon").classList = "ti ti-sun";
+    qi("toggleDarkIcon").style = "color: #fff;";
+    qi("toggleDarkIcon").classList = "ti ti-sun";
   }
 });
